86d151a6afc616b4aede8e77fc38fdc1463dddea
neeten97 <nathanieleeten@gmail.com>
2020-08-11 11:02:15 -0600

    id counter function and actualy adding registered user to db

--- userService/data.py
+++ userService/data.py
@@ -26,6 +26,20 @@ def register(username, password):
         code = 409
         return status, code
     
+    users.insert_one({'_id': get_id_counter(), 'username': username, 'password': password})
+
     status = 'User registered'
     code = 201
     return status, code
+
+def get_id_counter():
+    '''This function will get a unique ID by pulling it from the counter field of a counter
+    document, then increase the counter value.'''
+    return util.find_one_and_update({'_id': 'ID_COUNTER'},
+                                    {'$inc': {'count': 1}},
+                                    return_document=pymongo.ReturnDocument.AFTER)['count']
+
+if __name__ == '__main__':
+    users.drop()
+
+    users.insert_one({'_id': 'ID_COUNTER', 'count': 0})
