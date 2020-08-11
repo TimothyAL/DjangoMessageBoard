2db5f71762acf73f70ad570435c8e02548a86f45
neeten97 <nathanieleeten@gmail.com>
2020-08-11 13:30:39 -0600

    chat works

--- dmService/index.js
+++ dmService/index.js
@@ -1,13 +1,25 @@
-var app = require('express')();
+var app = require('express')(); // Only used to send garbage test client
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
-    console.log('A user connected')
+
+    console.log(`User ${socket.id} connected`)
+
     socket.on('disconnect', () => {
         console.log('A user disconnected')
     })
+
+    socket.on('clientMessage', (msg) => {
+        console.log(msg)
+        io.emit('serverMessage', msg)
+    })
+
 })
+
+app.get('/', (req, res) => {                // Garbage test client send
+  res.sendFile(__dirname + '/index.html');
+});
 
 http.listen(40000, () => {
     console.log('Listening on *:40000')
